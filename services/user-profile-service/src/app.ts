c8c42710be15f68cc5c969d95e55065313f4c932
NguyenThanhTrung-N2T <23521683@gm.uit.edu.vn>
2025-09-30 01:05:10 +0700

    set up package

---
+++ services/user-profile-service/src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database connection
+// AppDataSource.initialize()
+//   .then(() => {
+//     console.log("Database connected successfully");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to database. Details:", {
+//       message: error.message,
+//       code: error.code,
+//       stack: error.stack,
+//     });
+//     process.exit(1);
+//   });
+
+// Health check route
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    service: "Auth Service",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.use(express.json());
+
+// Routes
+//app.use("/api/auth", authRoutes);
+
+// Error handling middleware
+//app.use(serviceErrorHandler);
+//app.use(errorHandler);
+
+export default app;
