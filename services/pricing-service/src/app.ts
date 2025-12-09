35598f30602ab5143620c776796c2e7136a9604d
NguyenThanhTrung-N2T <23521683@gm.uit.edu.vn>
2025-12-10 02:39:26 +0700

    run test service

--- services/pricing-service/src/app.ts
+++ services/pricing-service/src/app.ts
@@ -1,39 +1,39 @@
 import express from "express";
-// import cors from "cors";
-// import { AppDataSource } from "./config/database";
+import cors from "cors";
+import { AppDataSource } from "./config/database";
 // import authRoutes from "./routes/auth.route";
 // import { errorHandler } from "./middlewares/errorHandler";
 // import { serviceErrorHandler } from "./middlewares/serviceErrorHandler";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 const app = express();
 
-// //Middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(cookieParser());
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 //Database connection
-// AppDataSource.initialize()
-//   .then(() => {
-//     console.log("Database connected successfully");
-//   })
-//   .catch((error) => {
-//     console.error("Error connecting to database. Details:", {
-//       message: error.message,
-//       code: error.code,
-//       stack: error.stack,
-//     });
-//     process.exit(1);
-//   });
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to database. Details:", {
+      message: error.message,
+      code: error.code,
+      stack: error.stack,
+    });
+    process.exit(1);
+  });
 
-// // Health check route
-// app.get("/", (req, res) => {
-//   res.json({
-//     status: "OK",
-//     service: "Pricing Service",
-//     timestamp: new Date().toISOString(),
-//   });
-// });
+// Health check route
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    service: "Pricing Service",
+    timestamp: new Date().toISOString(),
+  });
+});
 
 app.use(express.json());
 
