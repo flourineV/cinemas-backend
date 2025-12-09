aa59333c38818fb4cc0da7307a719fe6f6170e18
NguyenThanhTrung-N2T <23521683@gm.uit.edu.vn>
2025-12-10 02:35:22 +0700

    setup and config for pricing service
    - add config DB
    - add models, service

---
+++ services/pricing-service/src/app.ts
@@ -0,0 +1,47 @@
+import express from "express";
+// import cors from "cors";
+// import { AppDataSource } from "./config/database";
+// import authRoutes from "./routes/auth.route";
+// import { errorHandler } from "./middlewares/errorHandler";
+// import { serviceErrorHandler } from "./middlewares/serviceErrorHandler";
+// import cookieParser from "cookie-parser";
+const app = express();
+
+// //Middleware
+// app.use(cors());
+// app.use(express.json());
+// app.use(cookieParser());
+
+//Database connection
+// AppDataSource.initialize()
+//   .then(() => {
+//     console.log("Database connected successfully");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to database. Details:", {
+//       message: error.message,
+//       code: error.code,
+//       stack: error.stack,
+//     });
+//     process.exit(1);
+//   });
+
+// // Health check route
+// app.get("/", (req, res) => {
+//   res.json({
+//     status: "OK",
+//     service: "Pricing Service",
+//     timestamp: new Date().toISOString(),
+//   });
+// });
+
+app.use(express.json());
+
+// Routes
+// app.use("/api/auth", authRoutes);
+
+// Error handling middleware
+// app.use(serviceErrorHandler);
+// app.use(errorHandler);
+
+export default app;
---
+++ services/pricing-service/src/models/SeatPrice.entity.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "seat_price" })
+export class SeatPrice {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ name: "seat_type", length: 50 })
+  seatType: string;
+
+  @Column({ name: "ticket_type", length: 50 })
+  ticketType: string;
+
+  @Column("decimal", { name: "base_price", precision: 10, scale: 2 })
+  basePrice: number;
+
+  @Column({ name: "description", length: 255, nullable: true })
+  description: string;
+}
